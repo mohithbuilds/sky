9c73ac250b73b2d5a2ba431f13ffb49e3868bc1f
mohithn04 <mohith.n2022@gmail.com>
2026-01-20 00:55:46 -0800

    Add get air quality feature and relevant tests

---
+++ internal/client/openmateo/airquality.go
@@ -0,0 +1,82 @@
+package openmateo
+
+import (
+	"encoding/json"
+	"fmt"
+	"strings"
+)
+
+// Only extracting the wanted parts of the air quality API response
+type Result struct {
+	GenerationTimeMs float64     `json:"generationtime_ms"`
+	Hourly           Hourly      `json:"hourly"`
+	HourlyUnits      HourlyUnits `json:"hourly_units"`
+}
+
+// Hourly contains the time-series data for each hourly air quality parameter
+// Pollen only available for Europe
+type Hourly struct {
+	Time                []string  `json:"time,omitempty"`
+	PM10                []float64 `json:"pm10,omitempty"`
+	PM2_5               []float64 `json:"pm2_5,omitempty"`
+	CarbonMonoxide      []float64 `json:"carbon_monoxide,omitempty"`
+	NitrogenDioxide     []float64 `json:"nitrogen_dioxide,omitempty"`
+	SulphurDioxide      []float64 `json:"sulphur_dioxide,omitempty"`
+	Ozone               []float64 `json:"ozone,omitempty"`
+	AerosolOpticalDepth []float64 `json:"aerosol_optical_depth,omitempty"`
+	Dust                []float64 `json:"dust,omitempty"`
+	UVIndex             []float64 `json:"uv_index,omitempty"`
+	AlderPollen         []float64 `json:"alder_pollen,omitempty"`
+	BirchPollen         []float64 `json:"birch_pollen,omitempty"`
+	GrassPollen         []float64 `json:"grass_pollen,omitempty"`
+	MugwortPollen       []float64 `json:"mugwort_pollen,omitempty"`
+	OlivePollen         []float64 `json:"olive_pollen,omitempty"`
+	RagweedPollen       []float64 `json:"ragweed_pollen,omitempty"`
+}
+
+// HourlyUnits contains the units for each hourly air quality parameter
+type HourlyUnits struct {
+	Time                string `json:"time,omitempty"`
+	PM10                string `json:"pm10,omitempty"`
+	PM2_5               string `json:"pm2_5,omitempty"`
+	CarbonMonoxide      string `json:"carbon_monoxide,omitempty"`
+	NitrogenDioxide     string `json:"nitrogen_dioxide,omitempty"`
+	SulphurDioxide      string `json:"sulphur_dioxide,omitempty"`
+	Ozone               string `json:"ozone,omitempty"`
+	AerosolOpticalDepth string `json:"aerosol_optical_depth,omitempty"`
+	Dust                string `json:"dust,omitempty"`
+	UVIndex             string `json:"uv_index,omitempty"`
+	AlderPollen         string `json:"alder_pollen,omitempty"`
+	BirchPollen         string `json:"birch_pollen,omitempty"`
+	GrassPollen         string `json:"grass_pollen,omitempty"`
+	MugwortPollen       string `json:"mugwort_pollen,omitempty"`
+	OlivePollen         string `json:"olive_pollen,omitempty"`
+	RagweedPollen       string `json:"ragweed_pollen,omitempty"`
+}
+
+func (aqc *AirQualityClient) GetAirQuality(
+	longitude, latitude float64,
+	hourlyAirQualityParameters []string,
+) (*Result, error) {
+	if len(hourlyAirQualityParameters) == 0 {
+		hourlyAirQualityParameters = []string{"pm10", "pm2_5"}
+	}
+	airQualityURL := fmt.Sprintf(
+		"%sair-quality?latitude=%f&longitude=%f&hourly=%s",
+		aqc.BaseURL,
+		latitude,
+		longitude,
+		strings.Join(hourlyAirQualityParameters, ","),
+	)
+
+	data, err := aqc.doRequest(airQualityURL)
+	if err != nil {
+		return nil, fmt.Errorf("failed to get air quality data: %w", err)
+	}
+
+	var result Result
+	if err := json.Unmarshal(data, &result); err != nil {
+		return nil, fmt.Errorf("failed to unmarshal air quality data: %w", err)
+	}
+	return &result, nil
+}
